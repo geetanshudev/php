6eab1379af518ef731382cc5fde18266d64c1eb4
Geetanshu Dev <119582068+geetanshudev@users.noreply.github.com>
2022-12-28 16:43:21 +0530

    Add files via upload

---
+++ program1.php
@@ -0,0 +1,11 @@
+<?php
+	echo "hello world";
+	$variable1 = 177;
+	$variable2 = 78;
+	echo $variable1;
+	echo "</br>";
+	echo $variable2;
+	echo "</br>";
+	echo "addition of two variables=".$variable1 + $variable2;
+	
+?>
